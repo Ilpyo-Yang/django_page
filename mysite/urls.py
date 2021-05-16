556821399cb9f834aa9e6f5264401549cdfe6a7e
Ilpyo-Yang <ilpyoyang@gmail.com>
2021-05-16 22:27:03 +0900

    파일 네이밍 변경

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("ilpyo/", include("ilpyo.urls")),
+    path("ilpyo/", include("config.urls")),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
